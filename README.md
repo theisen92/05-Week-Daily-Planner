4138556a68834258446ea5ebe46db7ea20699841
theisen92 <andrew.peter.theisen@gmail.com>
2020-08-05 22:30:26 -0500

    Was able to get the local storage working and the daily planner is fully functional

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ https://theisen92.github.io/05-Week-Daily-Planner/daily-planner.html
 
 ## Usage
 
-Setting up a day to day schedule
+Setting up a hourly tasks to keep up with daily goals or work dead-lines
 
 ## Credits
 
@@ -18,10 +18,12 @@ I primarily used the information from MDN Developer and full stack for resources
 
 ## Contributing
 
-I worked on the project alone.
+I worked on the project by myself with help from Nate and Johnny.
 
 ## Challenges
 
-The main challenge that I was running into during this project was switching the divs into textareas then back, updating the background colors and storing the data when reloaded.
-I managed to find a way to update the textareas and progress the time. But I wasn't able to figure out the local storage and how to display it in the time slots.
-I'm planning on going over it with a TA durring next class so hopfully we'll get it figured out.
+The main challenge that I was running into during this project was updating the background colors and storing the data when reloaded.
+I managed to find a way to update the textareas and progress the time. Although I feel there is a simpler way using nextAll and prevAll.
+I was also having a hard time figuring out the local storage and how to display it in the time slots after reload.
+I went over it with a TA during after class with Nate and Johnny and I was making it a lot more complicated then it had to be.
+They were able to give me good next steps and worked through getting one of the time slots working.
--- script.js
+++ script.js
@@ -6,144 +6,142 @@ var currentTime = parseInt($(".hour").text());
 
 var timePars = parseInt(timeCouter);
 
+// var valueStore = JSON.parse(localStorage.values);
+
 $("#currentDay").text(dayCouter);
 
 var viewableText;
 
-if (timePars <= 8) {
-  $("#9-block").addClass("future");
-  $("#10-block").addClass("future");
-  $("#11-block").addClass("future");
-  $("#12-block").addClass("future");
-  $("#1-block").addClass("future");
-  $("#2-block").addClass("future");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 9) {
-  $("#9-block").addClass("present");
-  $("#10-block").addClass("future");
-  $("#11-block").addClass("future");
-  $("#12-block").addClass("future");
-  $("#1-block").addClass("future");
-  $("#2-block").addClass("future");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 10) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("present");
-  $("#11-block").addClass("future");
-  $("#12-block").addClass("future");
-  $("#1-block").addClass("future");
-  $("#2-block").addClass("future");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 11) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("present");
-  $("#12-block").addClass("future");
-  $("#1-block").addClass("future");
-  $("#2-block").addClass("future");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 12) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("present");
-  $("#1-block").addClass("future");
-  $("#2-block").addClass("future");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 13) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("past");
-  $("#1-block").addClass("present");
-  $("#2-block").addClass("future");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 14) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("past");
-  $("#1-block").addClass("past");
-  $("#2-block").addClass("present");
-  $("#3-block").addClass("future");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 15) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("past");
-  $("#1-block").addClass("past");
-  $("#2-block").addClass("past");
-  $("#3-block").addClass("present");
-  $("#4-block").addClass("future");
-  $("#5-block").addClass("future");
-} else if (timePars <= 16) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("past");
-  $("#1-block").addClass("past");
-  $("#2-block").addClass("past");
-  $("#3-block").addClass("past");
-  $("#4-block").addClass("present");
-  $("#5-block").addClass("future");
-} else if (timePars <= 16) {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("past");
-  $("#1-block").addClass("past");
-  $("#2-block").addClass("past");
-  $("#3-block").addClass("past");
-  $("#4-block").addClass("past");
-  $("#5-block").addClass("present");
-} else {
-  $("#9-block").addClass("past");
-  $("#10-block").addClass("past");
-  $("#11-block").addClass("past");
-  $("#12-block").addClass("past");
-  $("#1-block").addClass("past");
-  $("#2-block").addClass("past");
-  $("#3-block").addClass("past");
-  $("#4-block").addClass("past");
-  $("#5-block").addClass("past");
-}
-
-function textClicked() {
-  var textHtml = $(this).html();
-  var editableText = $("<textarea />");
-  editableText.val(textHtml);
-  $(this).replaceWith(editableText);
-  editableText.focus();
-  editableText.blur(editableTextBlurred);
-}
-
-function editableTextBlurred() {
-  var html = $(this).val();
-  viewableText = $("<div>");
-  viewableText.addClass("col-lg-9 text-area");
-  viewableText.html(html);
-  $(this).replaceWith(viewableText);
-  viewableText.click(textClicked);
-}
-
 $(document).ready(function () {
-  $(".text-area").click(textClicked);
+  $("#9-block .description").val(localStorage.getItem("9-block"));
+  $("#10-block .description").val(localStorage.getItem("10-block"));
+  $("#11-block .description").val(localStorage.getItem("11-block"));
+  $("#12-block .description").val(localStorage.getItem("12-block"));
+  $("#1-block .description").val(localStorage.getItem("1-block"));
+  $("#2-block .description").val(localStorage.getItem("2-block"));
+  $("#3-block .description").val(localStorage.getItem("3-block"));
+  $("#4-block .description").val(localStorage.getItem("4-block"));
+  $("#5-block .description").val(localStorage.getItem("5-block"));
+
+  if (timePars <= 8) {
+    $("#9-block").addClass("future");
+    $("#10-block").addClass("future");
+    $("#11-block").addClass("future");
+    $("#12-block").addClass("future");
+    $("#1-block").addClass("future");
+    $("#2-block").addClass("future");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 9) {
+    $("#9-block").addClass("present");
+    $("#10-block").addClass("future");
+    $("#11-block").addClass("future");
+    $("#12-block").addClass("future");
+    $("#1-block").addClass("future");
+    $("#2-block").addClass("future");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 10) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("present");
+    $("#11-block").addClass("future");
+    $("#12-block").addClass("future");
+    $("#1-block").addClass("future");
+    $("#2-block").addClass("future");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 11) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("present");
+    $("#12-block").addClass("future");
+    $("#1-block").addClass("future");
+    $("#2-block").addClass("future");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 12) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("present");
+    $("#1-block").addClass("future");
+    $("#2-block").addClass("future");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 13) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("past");
+    $("#1-block").addClass("present");
+    $("#2-block").addClass("future");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 14) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("past");
+    $("#1-block").addClass("past");
+    $("#2-block").addClass("present");
+    $("#3-block").addClass("future");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 15) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("past");
+    $("#1-block").addClass("past");
+    $("#2-block").addClass("past");
+    $("#3-block").addClass("present");
+    $("#4-block").addClass("future");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 16) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("past");
+    $("#1-block").addClass("past");
+    $("#2-block").addClass("past");
+    $("#3-block").addClass("past");
+    $("#4-block").addClass("present");
+    $("#5-block").addClass("future");
+  } else if (timePars <= 16) {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("past");
+    $("#1-block").addClass("past");
+    $("#2-block").addClass("past");
+    $("#3-block").addClass("past");
+    $("#4-block").addClass("past");
+    $("#5-block").addClass("present");
+  } else {
+    $("#9-block").addClass("past");
+    $("#10-block").addClass("past");
+    $("#11-block").addClass("past");
+    $("#12-block").addClass("past");
+    $("#1-block").addClass("past");
+    $("#2-block").addClass("past");
+    $("#3-block").addClass("past");
+    $("#4-block").addClass("past");
+    $("#5-block").addClass("past");
+  }
 
   $(".saveBtn").on("click", function () {
-    console.log(viewableText.text());
+    var siblingText = $(this).siblings(".description").val();
+    var timeBlock = $(this).parent().attr("id");
+    console.log(timeBlock);
+    // console.log(viewableText.text());
+    console.log($(this).siblings());
+    console.log(siblingText);
+    localStorage.setItem(timeBlock, siblingText);
   });
 });
